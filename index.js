1d5142f7fa252a5a497ee0ebc8444737ed410861
Artemet <60576459+Artemet@users.noreply.github.com>
2021-03-05 12:19:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,5 +11,118 @@ function getMenu() {
 }
 
 function send() {
-    console.log("Спасибо!")
+    console.log("Спасибо!");
+}
+
+function draw_python() {
+    console.log("Как рисовать на python?");
+}
+
+function what_github() {
+    console.log("Что такое git hub?");
+}
+
+function game_python() {
+    console.log("Игра на python");
+}
+
+function lern_javaScript_four() {
+    console.log("Изучение JavaScript часть 4");
+}
+
+function programm_arr() {
+    console.log("Программа со списками");
+}
+
+function lern_java() {
+    console.log("Изучение java часть 2");
+}
+
+function lern_javaScript_five() {
+    console.log("Изучение JavaScript часть 5");
+}
+
+function programm_python_one() {
+    console.log("Программа на python часть 1");
+}
+
+function lesson_unity_one() {
+    console.log("Изучение unity часть 1");
+}
+
+function lesson_cSharp_three() {
+    console.log("Изучение c# часть 3");
+}
+
+function lesson_javaScript_three() {
+    console.log("Изучение JavaScript часть 3");
+}
+
+function lesson_cSharp_two() {
+    console.log("Изучение c# часть 2");
+}
+
+function code_javaScript() {
+    console.log("Можно ли кодить только на JS?");
+}
+
+function copy_veb_three() {
+    console.log("Копирование сайтов часть 3");
+}
+
+function lern_python_three() {
+    console.log("Изучение python часть 3");
+}
+
+function menu_javaScript() {
+    console.log("Как сделать менюшку на JS!");
+}
+
+function lern_cSharp_one() {
+    console.log("Изучение c# часть 1");
+}
+
+function lern_javaScript_one() {
+    console.log("Изучение JavaScript часть 1")
+}
+
+function lern_python_two() {
+    console.log("Изучение python часть 2");
+}
+
+function copy_veb_two() {
+    console.log("Копирование сайтов часть 2");
+}
+
+function copy_python_one() {
+    console.log("Изучение python часть 1");
+}
+
+function copy_veb_one() {
+    console.log("Копирование сайтов часть 1");
+}
+
+function lern_veb_one() {
+    console.log("Изучение HTML часть 1");
+}
+
+function veb_js_one() {
+    console.log("Делаю сайт только на Java Script часть 1");
+}
+
+function lovar_py() {
+    console.log("Программа со словариками на python");
+}
+
+function click_color() {
+    document.getElementById("menu_color");
+    menu_color.style.color = "greenyellow";
+}
+
+function epic_game_download() {
+    console.log("Подготавливаемся к разработке игр");
+}
+
+function epic_game_play(){
+    console.log("Пробую делать игру паркур");
 }
